0527a60ae54e98897faf66f8924e5909557aae63
Alex Macocian <amacocian@yahoo.com>
2019-07-18 15:49:50 +0300

    Changed target to all

---
+++ MTSC-TestClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MTSC_TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
