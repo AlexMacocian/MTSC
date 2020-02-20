2ac5559e759004f20ab5727c9de910334de86944
Alexandru Macocian <amacocian@yahoo.com>
2020-02-20 17:09:15 +0100

    Added server reference in route enablers for logging purposes

--- MTSC/Common/Http/ServerModules/HttpRoutingModule.cs
+++ MTSC/Common/Http/ServerModules/HttpRoutingModule.cs
@@ -9,23 +9,19 @@ namespace MTSC.Common.Http.ServerModules
 {
     public class HttpRoutingModule : IHttpModule
     {
-        private Dictionary<HttpMethods, Dictionary<string, (IHttpRoute, Func<HttpRequest, ClientData, bool>)>> moduleDictionary = 
-            new Dictionary<HttpMethods, Dictionary<string, (IHttpRoute, Func<HttpRequest, ClientData, bool>)>>();
+        private Dictionary<HttpMethods, Dictionary<string, (IHttpRoute, Func<Server.Server, HttpRequest, ClientData, bool>)>> moduleDictionary = 
+            new Dictionary<HttpMethods, Dictionary<string, (IHttpRoute, Func<Server.Server, HttpRequest, ClientData, bool>)>>();
 
         public HttpRoutingModule()
         {
             foreach (HttpMethods method in (HttpMethods[])Enum.GetValues(typeof(HttpMethods)))
             {
-                moduleDictionary[method] = new Dictionary<string, (IHttpRoute, Func<HttpRequest, ClientData, bool>)>();
+                moduleDictionary[method] = new Dictionary<string, (IHttpRoute, Func<Server.Server, HttpRequest, ClientData, bool>)>();
             }
         }
 
-        public HttpRoutingModule AddRoute(HttpMethods method, string uri, IHttpRoute routeModule, Func<HttpRequest, ClientData, bool> routeEnabler = null)
+        public HttpRoutingModule AddRoute(HttpMethods method, string uri, IHttpRoute routeModule, Func<Server.Server, HttpRequest, ClientData, bool> routeEnabler = null)
         {
-            if(routeEnabler == null)
-            {
-                routeEnabler = (request, client) => { return true; };
-            }
             moduleDictionary[method][uri] = (routeModule, routeEnabler);
             return this;
         }
@@ -38,7 +34,7 @@ namespace MTSC.Common.Http.ServerModules
             if (moduleDictionary[request.Method].ContainsKey(request.RequestURI))
             {
                 (var module, var routeEnabler) = moduleDictionary[request.Method][request.RequestURI];
-                if(routeEnabler.Invoke(request, client))
+                if(routeEnabler == null || routeEnabler.Invoke(server, request, client))
                 {
                     try
                     {
