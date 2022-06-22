67257f56088b1acbd520ed5268b3a60a6a22362e
Alexandru Macocian <amacocian@yahoo.com>
2022-06-22 16:56:19 +0200

    Setup authorizeattribute tests

--- MTSC.OAuth2.Tests/UnitTests/AuthorizeAttributeTests.cs
+++ MTSC.OAuth2.Tests/UnitTests/AuthorizeAttributeTests.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MTSC.OAuth2.Attributes;
+using MTSC.OAuth2.Authorization;
 
 namespace MTSC.OAuth2.Tests.UnitTests
 {
     [TestClass]
     public sealed class AuthorizeAttributeTests
     {
+        private readonly Mock<ILogger<AuthorizeAttribute>> loggerMock = new();
+        private readonly Mock<IAuthorizationProvider> authorizationProviderMock = new();
+        private readonly AuthorizeAttribute authorizeAttribute;
+
+        public AuthorizeAttributeTests()
+        {
+            this.authorizeAttribute = new AuthorizeAttribute(this.authorizationProviderMock.Object, this.loggerMock.Object);
+        }
     }
 }
